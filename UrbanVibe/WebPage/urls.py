5edb2ff4bb0ad74d95936967bc709bcd3cc9ce4c
Samuel Jethro <hubertus.samuel.jethro@gmail.com>
2025-02-22 23:45:00 +0700

    about us page

--- UrbanVibe/WebPage/urls.py
+++ UrbanVibe/WebPage/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about_us/', views.about_us, name='about_us'),  # About Us page
 ]
--- UrbanVibe/WebPage/views.py
+++ UrbanVibe/WebPage/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'WebPage/index.html')
+
+def about_us(request):
+    return render(request, 'WebPage/about_us.html')
